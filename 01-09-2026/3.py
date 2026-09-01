c0572ddc59a774f5d5b9c0481a780514127f800f
Parth Nanda <parthnanda2024@gmail.com>
2026-09-01 12:44:35 +0530

    2

--- 01-09-2026/3.py
+++ 01-09-2026/3.py
@@ -2,3 +2,9 @@
 t=input("Enter a string:")
 #print(" " in t)
 print(t.find(" "))
+
+
+#4. Write a Python program to replace double spaces from problem 3 with single spaces.
+t=input("Enter a string:")
+t=t.replace("  "," ")
+print(t)
--- 01-09-2026/4.py
+++ 01-09-2026/4.py
@@ -1,3 +1,4 @@
 #4. Write a Python program to replace double spaces from problem 3 with single spaces.
 t=input("Enter a string:")
-print(" " in t)
+t=t.replace("  "," ")
+print(t)
